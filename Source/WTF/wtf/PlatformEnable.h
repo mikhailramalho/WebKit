ada3c94f3082d6a9a44ea40e7e87e681a3d8fa02
Mikhail R. Gadelha <mikhail@igalia.com>
2022-06-30 21:33:49 +0000

    Enable concurrency for all
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- Source/WTF/wtf/PlatformEnable.h
+++ Source/WTF/wtf/PlatformEnable.h
@@ -666,7 +666,7 @@
    values get stored to atomically. This is trivially true on 64-bit platforms,
    but not true at all on 32-bit platforms where values are composed of two
    separate sub-values. */
-#if ENABLE(JIT) && USE(JSVALUE64)
+#if ENABLE(JIT)
 #define ENABLE_CONCURRENT_JS 1
 #endif
 
